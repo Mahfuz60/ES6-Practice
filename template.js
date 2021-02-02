0aec2e0961e81913dad9cdc9b5a6bba1ddefae92
Mahfuz <you@example.com>
2021-02-02 15:31:25 +0600

    codeup

---
+++ template.js
@@ -0,0 +1,21 @@
+const firstName="Mahfuz";
+const lastName= "Alam";
+const fullName=firstName+" "+lastName+"is good boy all time";
+console.log(fullName);
+//ES6 template used 
+const fullName2=`${firstName} ${30+50+90} tk collect money at office.`;
+console.log(fullName2);
+
+const multiLine="I love you\n"
+    +"I miss you\n"
+    +"I need you all time ";
+    console.log(multiLine);
+
+    //used template
+
+    const multiLine2=`
+    I love you.
+    I miss you.
+    I don't need you,because I manage you self confidence`;
+                      
+    console.log(multiLine2);
